1f58b74a1b8528befa6665e3884fb10c958ace42
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-05-23 21:59:10 +0200

    Use a i64 to compute the base36 string

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@ pub struct Base36 {}
 const ZERO: &str = "0";
 const MINUS: char = '-';
 const CHARSET: &str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const CHARSET_LENGTH: i32 = 36;
 
 impl Base36 {
     pub fn encode(number: i32) -> String {
-        let mut number = number;
+        let mut number = i64::from(number);
 
         let is_negative = number < 0;
         if is_negative {
@@ -18,16 +19,15 @@ impl Base36 {
         }
 
         let mut base_36 = Vec::new();
-        let charset_len = CHARSET.chars().count() as i32;
 
         while number > 0 {
             let char = CHARSET
                 .chars()
-                .nth((number % charset_len) as usize)
+                .nth((number % CHARSET_LENGTH as i64) as usize)
                 .unwrap();
 
             base_36.push(char);
-            number /= charset_len;
+            number /= CHARSET_LENGTH as i64;
         }
 
         let mut base_36 = base_36.into_iter().rev().collect::<String>();
@@ -83,6 +83,6 @@ mod tests {
     fn test_encode_min_i32() {
         let actual = Base36::encode(i32::MIN);
 
-        assert_eq!(actual, "-ZIK0ZJ");
+        assert_eq!(actual, "-ZIK0ZK");
     }
 }
