843625314bc536c91b734d682c97a5ef4bf54dfa
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-05-23 21:45:37 +0200

    Implement a Base36 struct with an encode method

---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+pub struct Base36 {}
+
+const ZERO: &str = "0";
+const MINUS: char = '-';
+const CHARSET: &str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+impl Base36 {
+    pub fn encode(number: i32) -> String {
+        let mut number = number;
+
+        let is_negative = number < 0;
+        if is_negative {
+            number = -number;
+        }
+
+        if number == 0 {
+            return String::from(ZERO);
+        }
+
+        let mut base_36 = Vec::new();
+        let charset_len = CHARSET.chars().count() as i32;
+
+        while number > 0 {
+            let char = CHARSET
+                .chars()
+                .nth((number % charset_len) as usize)
+                .unwrap();
+
+            base_36.push(char);
+            number /= charset_len;
+        }
+
+        let mut base_36 = base_36.into_iter().rev().collect::<String>();
+
+        if is_negative {
+            base_36.insert(0, MINUS);
+        }
+
+        base_36
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Base36;
+
+    #[test]
+    fn test_encode_zero() {
+        let actual = Base36::encode(0);
+
+        assert_eq!(actual, "0");
+    }
+
+    #[test]
+    fn test_encode_positive_number() {
+        let actual = Base36::encode(12345);
+
+        assert_eq!(actual, "9IX");
+    }
+
+    #[test]
+    fn test_encode_negative_number() {
+        let actual = Base36::encode(-9876);
+
+        assert_eq!(actual, "-7MC");
+    }
+
+    #[test]
+    fn test_encode_large_number() {
+        let actual: String = Base36::encode(987654321);
+
+        assert_eq!(actual, "GC0UY9");
+    }
+
+    #[test]
+    fn test_encode_max_i32() {
+        let actual: String = Base36::encode(i32::MAX);
+
+        assert_eq!(actual, "ZIK0ZJ");
+    }
+
+    #[test]
+    fn test_encode_min_i32() {
+        let actual = Base36::encode(i32::MIN);
+
+        assert_eq!(actual, "-ZIK0ZJ");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{env, error::Error, process::exit};
 
+use base36::*;
+
 fn parse_args() -> Result<i32, Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
@@ -18,31 +20,9 @@ fn parse_args() -> Result<i32, Box<dyn Error>> {
     Ok(number)
 }
 
-fn to_base36(number: i32) -> String {
-    let mut number = number;
-    let charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let charset_len = charset.len() as i32;
-
-    let mut text = String::new();
-
-    let num_digits = number.ilog10() / charset_len.ilog10();
-
-    for _ in 0..num_digits {
-        let char = charset
-            .chars()
-            .nth((number % charset_len) as usize)
-            .unwrap();
-
-        text.push(char);
-        number /= charset_len;
-    }
-
-    text
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let number = parse_args()?;
-    let in_base36 = to_base36(number);
+    let in_base36 = Base36::encode(number);
 
     println!("{}", in_base36);
     Ok(())
